b508688c1a115b1d2e75b62418c72bf949f7b816
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2022-06-21 18:31:15 +0100

    updated build

--- index.ts
+++ index.ts
@@ -4,4 +4,13 @@ import { ConsumerEvents } from './src/models/consumer-events';
 import { KafkaTopic } from './src/models/kafka-topics';
 import { IKafKaConfig } from './src/models/kafka-config';
 import { IKafkaMessage, IKafkaMessageHandler } from './src/models/i-kafka-message';
-export { KafkaService, ProducerEvents, ConsumerEvents, IKafkaMessage, IKafkaMessageHandler, KafkaTopic, IKafKaConfig, };
+
+export {
+  KafkaService,
+  ProducerEvents,
+  ConsumerEvents,
+  IKafkaMessage,
+  IKafkaMessageHandler,
+  KafkaTopic,
+  IKafKaConfig,
+};
