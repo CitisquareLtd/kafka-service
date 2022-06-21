fe92a72f0b09138f43ac649b7c6acc73ccd73b4c
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2022-06-21 18:05:21 +0100

    updated build

---
+++ dist/models/kafka-topics.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.KafkaTopic = void 0;
+var KafkaTopic;
+(function (KafkaTopic) {
+    KafkaTopic["NOTIFICATION"] = "notification";
+    KafkaTopic["AUDIT_TRAIL"] = "audit-trail";
+})(KafkaTopic = exports.KafkaTopic || (exports.KafkaTopic = {}));
