750561091c50b2e4ac22e1492176023cb014180c
wiseminds <wiseminds@users.noreply.github.com>
2022-06-22 05:03:22 +0000

    deploy: 70bd54244b6d1b2a3f58979e2af87304131087a2

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import { KafkaService } from './src/kafka.service';
+import { ProducerEvents } from './src/models/producer-events';
+import { ConsumerEvents } from './src/models/consumer-events';
+import { KafkaTopic } from './src/models/kafka-topics';
+import { IKafKaConfig } from './src/models/kafka-config';
+import { IKafkaMessage, IKafkaMessageHandler } from './src/models/i-kafka-message';
+export { KafkaService, ProducerEvents, ConsumerEvents, IKafkaMessage, IKafkaMessageHandler, KafkaTopic, IKafKaConfig, };
